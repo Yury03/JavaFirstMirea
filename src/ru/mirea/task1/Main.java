e18b4602d31d7f07fee5428d4e1b960092b588ee
Yury03 <yrvish03@bk.ru>
2022-09-17 00:04:22 +0300

    test

--- src/ru/mirea/task1/Main.java
+++ src/ru/mirea/task1/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         workWithArray();
-
+//test
     }
     public static void workWithArray(){
         Scanner in = new Scanner(System.in);
